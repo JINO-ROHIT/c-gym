302b235d2c408f7793ad1c453241c8bdb7ff8259
JINO-ROHIT <find.jinorohit@gmail.com>
2025-11-17 22:24:47 +0300

    multiple type templates

---
+++ concepts/templates_multiple_types.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+template <typename T, typename U> // We're using two template type parameters named T and U
+T max(T x, U y) // x can resolve to type T, and y can resolve to type U
+{
+    return (x < y) ? y : x; // uh oh, we have a narrowing conversion problem here
+}
+
+int main()
+{
+    std::cout << max(2, 3.5) << '\n'; // resolves to max<int, double>
+
+    return 0;
+}
+
+// this program actually return 3 bc check the return type T
+
+
+// try using auto
+
+#include <iostream>
+
+template <typename T, typename U>
+auto max(T x, U y) // ask compiler can figure out what the relevant return type is
+{
+    return (x < y) ? y : x;
+}
+
+int main()
+{
+    std::cout << max(2, 3.5) << '\n';
+
+    return 0;
+}
