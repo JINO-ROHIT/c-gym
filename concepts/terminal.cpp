c71e7738aaa4d200a769c69676196768fba05e35
JINO-ROHIT <find.jinorohit@gmail.com>
2025-11-25 18:58:05 +0300

    terminal program

---
+++ concepts/terminal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    while(true){
+        cin >> age;
+        cout << "Age entered: " << age << endl;
+    }
+    return 0;
+}
