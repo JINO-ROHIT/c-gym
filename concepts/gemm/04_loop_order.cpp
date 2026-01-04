40daa55b7f63034ece55790fb4d28839264ab8d3
JINO-ROHIT <find.jinorohit@gmail.com>
2026-01-04 12:09:47 +0300

    gemm: loop re-order

---
+++ concepts/gemm/04_loop_order.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    const int rows = 2048;
+    const int cols = 2048;
+    const int stride = cols;
+
+    int* M = new int[rows * cols];
+    int* N = new int[rows * cols];
+    int* O = new int[rows * cols];
+    
+    for(int i = 0; i < rows * cols; i++){
+        M[i] = i % 10;
+        N[i] = (i + 1) % 10;
+        O[i] = 0;
+    }
+
+    cout << "\n\n" << "Result" << "\n\n";
+
+    auto start = chrono::high_resolution_clock::now();
+    // for(int i = 0; i < rows; i++){
+    //     for(int j = 0; j < cols; j++){
+    //         int sum = 0;
+    //         for(int k = 0; k < cols; k++){
+    //             sum += M[i * stride + k] * N[k * stride + j];
+    //             //cout << M[i * stride + k] << " " << N[k * stride + j] << " " << sum << endl; 
+    //         }
+    //         O[i * stride + j] = sum;
+    //     }
+    // }
+
+    for(int i = 0; i < rows; i++){
+        for(int k = 0; k < cols; k++){ // 'k' moved to the middle
+            int temp_M = M[i * stride + k]; // Load once, use for the whole 'j' loop
+            for(int j = 0; j < cols; j++){ // 'j' is now the innermost
+                O[i * stride + j] += temp_M * N[k * stride + j];
+            }
+        }
+    }
+    
+    auto end = chrono::high_resolution_clock::now();
+    auto naive_gemm_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+    cout << "naive GEMM: " << naive_gemm_time << " millioseconds\n";
+}
