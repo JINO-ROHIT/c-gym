b033cfe64485982d4b6923035c3747d619b9be79
JINO-ROHIT <find.jinorohit@gmail.com>
2026-01-06 16:15:06 +0300

    modern: value initialization

---
+++ concepts/modern_practices.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int hey{};  // prefer to use value initialization,
+    int hey2; 
+
+    cout << hey << endl;
+    cout << hey2 << endl;
+}
